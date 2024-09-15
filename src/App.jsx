539e59a9612d150c5c1a84fe580f37abc3fda11d
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-09-15 20:09:36 +0800

    style: Add background

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@
 import React, { useState } from "react";
 import "./App.css";
 import characters from "./characters";
-import Card from "./components/Card";
+import GameoverPage from "./pages/GameOverPage";
+import StartPage from "./pages/StartPage";
 
 function App() {
+  const five = characters.filter((_, idx) => idx < 5);
   return (
     <>
-      <h1>sasa</h1>
-      <div className="flex">
-        {characters.map((c) => (
-          <Card key={c.id} src={c.src}>
-            {c.name}
-          </Card>
-        ))}
+      <div className="flex flex-col min-h-screen min-w-full max-w-7xl">
+        {/* <StartPage /> */}
+        <GameoverPage />
       </div>
+      <div
+        className="min-h-full min-w-full  bg-no-repeat bg-cover bg-center absolute top-0 -z-10"
+        style={{
+          backgroundImage: "url(src/assets/background/at-bg.webp)",
+        }}
+      ></div>
     </>
   );
 }
