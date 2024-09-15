eb79e9ab0dbf7edf984a54ee3b252e15e1588ad6
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-09-15 20:13:34 +0800

    fix: Add media queries for large screen and add background

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,8 +13,8 @@ export default function Card({ src, children }) {
         setIsClick((c) => !c);
         setTimeout(() => setIsClick((c) => !c), 800);
       }}
-      className={`w-[180px] xl:w-[220px] ${isFlipY} transition-transform duration-1000 aspect-[1/1.546]  rounded-[1.25rem]`}
-      style={{ transformStyle: "preserve-3d" }}
+      className={`w-[180px] xl:w-[220px] ${isFlipY} cursor-pointer aspect-[1/1.546]  rounded-[1.25rem]`}
+      style={{ transition: "transform 1s", transformStyle: "preserve-3d" }}
     >
       <Tilt
         glareEnable={true}
@@ -27,28 +27,20 @@ export default function Card({ src, children }) {
       >
         <div
           id="card-face"
-          className="h-full w-full rounded-[1.25em] bg-[#ffffc4] p-3 absolute"
+          className="h-full w-full flex flex-col items-center rounded-[1.25em] bg-[#ffffc4] p-3 absolute"
         >
           <div
             style={{ backgroundImage: `url(${src})` }}
-            className="bg-cover bg-center rounded-[1.25em] min-h-full min-w-full"
-          >
-            {/* <img className="rounded-[1.25em]" src={src} alt="" /> */}
-          </div>
-          <div className="font-semibold">{children}</div>
+            className="bg-cover bg-center rounded-[1.25em] min-h-[calc(100%-1.5em)] min-w-full"
+          ></div>
+          <div className="font-semibold text-black">{children}</div>
         </div>
         <div
           id="card-back"
+          style={{ backgroundImage: "url(/src/assets/cards/bc.jpg)" }}
           className="h-full [backface-visibility:hidden] bg-cover bg-center  rounded-[1.25em] [transform:rotateY(180deg)]"
-          style={{ backgroundImage: "url(./assets/cards/bc.webp)" }}
-        >
-          card back
-        </div>
+        ></div>
       </Tilt>
     </div>
   );
 }
-
-// #ffffc4 bg card
-// aspect 1/1.546
-//220 - 180
