a81b357901ff6087c9e02ba6601d0f03a1972b8d
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-09-21 21:36:30 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/Denselle0311/react-memory-game/',
+  base: '/react-memory-game/',
 })
